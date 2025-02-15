96dfb44624404e1b1c42808299e0b5778c8937cd
Marat Zimnurov <zimtir@mail.ru>
2025-02-15 19:59:04 +0100

    build: add project configuration files

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  testEnvironment: "jsdom",
+  roots: ["./extension", "./tests"],
+  collectCoverage: true,
+  coverageReporters: ["json", "lcov", "text", "json-summary"],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["<rootDir>/./extension/*.js"],
+  moduleNameMapper: {
+    "^./extension/(.*)$": "<rootDir>/./extension/$1",
+  },
+  coveragePathIgnorePatterns: ["/node_modules/", "\\.test\\.js$"],
+  coverageProvider: "v8",
+  moduleDirectories: ["node_modules"],
+  setupFiles: ["<rootDir>/tests/mocks/chrome.js"],
+  testPathIgnorePatterns: ["/node_modules/"],
+
+  rootDir: __dirname,
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      statements: 100,
+      branches: 100,
+      // TODO: fix coverage
+      functions: 97.05,
+    },
+  },
+};
